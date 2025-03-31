149b04abc07e28ec2791067846e6df6b94a1fb62
MeetGoplani <meet.goplani@nerdshouse.com>
2025-03-31 16:08:27 +0530

    add

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -111,7 +111,7 @@ const ClickableImage = () => {
       <div className="w-full px-4 sm:px-6 md:px-8">
         <div className="relative w-full max-w-4xl mx-auto">
           <img
-            src="/images/splash.jpg"
+            src="/videos/splash.jpg"
             alt="Interactive Image"
             className="w-full h-auto"
           />
